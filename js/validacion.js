b50ffde7bdcf15944475416e4a22305324b64d6f
Jhonfer <fallen@fallen.fallen.com>
2021-01-07 22:05:35 -0500

    First Commit

---
+++ js/validacion.js
@@ -0,0 +1,18 @@
+var form  = document.getElementsByTagName('form')[0];
+var email = document.getElementById('mail');
+var error = document.querySelector('.error');
+
+email.addEventListener("input", function (event) {
+  if (email.validity.valid) {
+    error.innerHTML = ""; 
+    error.className = "error"; 
+  }
+}, false);
+
+form.addEventListener("submit", function (event) {
+  if (!email.validity.valid) {
+    error.innerHTML = "I expect an e-mail, darling!";
+    error.className = "error active";
+    event.preventDefault();
+  }
+}, false);
