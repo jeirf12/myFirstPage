8df1904f2e039cd08703fb677786b558ff3a13e1
jeirf12 <jeirf12@github.com>
2021-05-12 20:31:32 -0500

    updated arrow function

--- js/validacion.js
+++ js/validacion.js
@@ -2,14 +2,14 @@ var form  = document.getElementsByTagName('form')[0];
 var email = document.getElementById('mail')[0];
 var error = document.querySelector('.error');
 
-email.addEventListener("input", function (event) {
+email.addEventListener("input", (event) => {
   if (email.validity.valid) {
     error.innerHTML = ""; 
     error.className = "error"; 
   }
 }, false);
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", (event) => {
   if (!email.validity.valid) {
     error.innerHTML = "I expect an e-mail, darling!";
     error.className = "error active";
