4d8bb5728b366aef76c50aae4d2b7f4140aa7952
jeirf12 <jeirf12@github.com>
2021-05-14 22:51:34 -0500

    header responsive

---
+++ js/menu.js
@@ -0,0 +1,37 @@
+jQuery('document').ready(function($){
+  var menuBtn = $('.menu-icon'),
+    menu = $('.navigation ul'),
+    iniBtn = $('.inicio'),
+    acercaBtn = $('.acerca'),
+    servicioBtn = $('.servicio'),
+    portafolioBtn = $('.portafolio'),
+    contactoBtn = $('.contacto');
+
+  menuBtn.click(() =>{
+    if(menu.hasClass('show')){
+      menu.removeClass('show');
+    }else{
+      menu.addClass('show');
+    }
+  });
+
+  iniBtn.click(() =>{
+    menu.removeClass('show');
+  });
+
+  acercaBtn.click(() =>{
+    menu.removeClass('show')
+  });
+
+  servicioBtn.click(() =>{
+    menu.removeClass('show')
+  });
+  
+  portafolioBtn.click(() =>{
+    menu.removeClass('show')
+  });
+  
+  contactoBtn.click(() =>{
+    menu.removeClass('show')
+  });
+});
