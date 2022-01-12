7f17a416d01073338039161b2175cb512aa14b6b
jeirf12 <jeirf12@github.com>
2022-01-12 10:43:02 -0500

    update page responsive

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 jQuery('document').ready(function($){
   var menuBtn = $('.menu-icon'),
     menu = $('.navigation ul'),
+    header = $('.header'),
     iniBtn = $('.inicio'),
     acercaBtn = $('.acerca'),
     servicioBtn = $('.servicios'),
@@ -9,29 +10,36 @@ jQuery('document').ready(function($){
 
   menuBtn.click(() =>{
     if(menu.hasClass('show')){
+      header.removeClass('showheader')
       menu.removeClass('show');
     }else{
+      header.addClass('showheader')
       menu.addClass('show');
     }
   });
 
   iniBtn.click(() =>{
+    header.removeClass('showheader')
     menu.removeClass('show');
   });
 
   acercaBtn.click(() =>{
+    header.removeClass('showheader')
     menu.removeClass('show')
   });
 
   servicioBtn.click(() =>{
+    header.removeClass('showheader')
     menu.removeClass('show')
   });
   
   portafolioBtn.click(() =>{
+    header.removeClass('showheader')
     menu.removeClass('show')
   });
   
   contactoBtn.click(() =>{
+    header.removeClass('showheader')
     menu.removeClass('show')
   });
 });
--- js/validation.js
+++ js/validation.js
@@ -1,8 +1,8 @@
 var form  = document.getElementsByTagName('form')[0];
-var email = document.getElementById('mail')[0];
+var email = document.getElementById('mail');
 var error = document.querySelector('.error');
 
-email.addEventListener("input", (event) => {
+email.addEventListener("input", () => {
   if (email.validity.valid) {
     error.innerHTML = ""; 
     error.className = "error"; 
