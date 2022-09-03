01792f4bee7d94a549fafdd3df83a09b2de825df
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2022-09-02 19:06:26 -0500

    added news styles to flow page and added events new event click

--- js/LoadPage.js
+++ js/LoadPage.js
@@ -22,12 +22,26 @@ const openWindowNew = (url) => {
   window.open(url);
 }
 
+const addClass = (inputNew) => {
+  let inputsActive = document.getElementsByTagName("a");
+  for (let input of inputsActive) {
+    if(input.classList.contains("active")) input.classList.remove("active");
+  }
+  if (inputNew !== undefined) inputNew.classList.add("active");
+}
+
 loadClickEvent(facebook, openWindowNew, "https://www.facebook.com/Fallen.100101");
 loadClickEvent(instagram, openWindowNew, "https://www.instagram.com/fallen.1001");
 loadClickEvent(github, openWindowNew, "https://github.com/jeirf12");
 loadClickEvent(inicio[0], openLink, "#");
+loadClickEvent(inicio[0], addClass, undefined);
 loadClickEvent(inicio[1], openLink, "#");
+loadClickEvent(inicio[1], addClass, inicio[1]);
 loadClickEvent(acerca[0], openLink, `${nameHref}acerca`);
+loadClickEvent(acerca[0], addClass, acerca[0]);
 loadClickEvent(servicio[0], openLink, `${nameHref}servicios`);
+loadClickEvent(servicio[0], addClass, servicio[0]);
 loadClickEvent(portafolio[0], openLink, `${nameHref}portafolio`);
+loadClickEvent(portafolio[0], addClass, portafolio[0]);
 loadClickEvent(contacto[0], openLink, `${nameHref}contacto`);
+loadClickEvent(contacto[0], addClass, contacto[0]);
